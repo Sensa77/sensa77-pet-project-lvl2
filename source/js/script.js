3ca7dcc14daf41757272b68c8bf739d325671f5b
Sensa <77754618083@mail.ru>
2021-02-27 21:01:48 +0300

    Доделала интерактив, сделала js

---
+++ source/js/script.js
@@ -0,0 +1,54 @@
+const siteList = document.querySelector(".site-list");
+const buttonClose = document.querySelector(".site-list__close");
+const openList = document.querySelector(".main-nav__toggle");
+
+const showList = (event) => {
+  event.preventDefault();
+  siteList.classList.add("site-list--show");
+};
+
+const closeModal = (event) => {
+  event.preventDefault();
+  siteList.classList.remove("site-list--show");
+};
+
+openList.addEventListener("click", showList);
+buttonClose.addEventListener("click", closeModal);
+
+const submitButton = document.querySelector(".info__button");
+const submitModal = document.querySelector(".thanks");
+const closeButton = document.querySelector(".thanks__button");
+const errorModal = document.querySelector(".error");
+const errorButton = document.querySelector(".error__button");
+const nameFormInputs = document.querySelectorAll(".name-form input");
+const contactFormInputs = document.querySelectorAll(".number-form input");
+const inputs = [...nameFormInputs, ...contactFormInputs];
+
+const showPopup = (event) => {
+  event.preventDefault();
+  let error = false;
+  inputs.forEach((input) => {
+    if (!input.value) {
+      error = true;
+    }
+  });
+  if (error) {
+    errorModal.classList.add("error--show");
+  } else {
+    submitModal.classList.add("thanks--show");
+  }
+};
+
+const closeThanks = (event) => {
+  event.preventDefault();
+  submitModal.classList.remove("thanks--show");
+};
+
+const closeError = (event) => {
+  event.preventDefault();
+  errorModal.classList.remove("error--show");
+};
+
+submitButton.addEventListener("click", showPopup);
+closeButton.addEventListener("click", closeThanks);
+errorButton.addEventListener("click", closeError);
